761411103dfbb5d8217f78d96554799eacd65d6d
amine-abdeljaoued <amine@abdeljaoued.net>
2019-11-14 19:03:25 +0100

    Organizing code

--- campusmap/main.cpp
+++ campusmap/main.cpp
@@ -4,65 +4,13 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <iostream>
-
-class TileMap : public sf::Drawable, public sf::Transformable
-{
-public:
-
-    bool load(const std::string& tileset, sf::Vector2u tileSize, const int* tiles, unsigned int width, unsigned int height)
-    {
-        if (!m_tileset.loadFromFile(tileset))
-            return false;
-
-        m_vertices.setPrimitiveType(sf::Quads);
-        m_vertices.resize(width * height * 4);
-
-        for (unsigned int i = 0; i < width; ++i)
-            for (unsigned int j = 0; j < height; ++j)
-            {
-                int tileNumber = tiles[i + j * width];
-
-                int tu = tileNumber % (m_tileset.getSize().x / tileSize.x);
-                int tv = tileNumber / (m_tileset.getSize().x / tileSize.x);
-
-                sf::Vertex* quad = &m_vertices[(i + j * width) * 4];
-
-                quad[0].position = sf::Vector2f(i * tileSize.x , j * tileSize.y);
-                quad[1].position = sf::Vector2f((i + 1) * tileSize.x, j * tileSize.y);
-                quad[2].position = sf::Vector2f((i + 1) * tileSize.x, (j + 1) * tileSize.y  );
-                quad[3].position = sf::Vector2f(i * tileSize.x  , (j + 1) * tileSize.y );
-
-                quad[0].texCoords = sf::Vector2f(tu * tileSize.x , tv * tileSize.y);
-                quad[1].texCoords = sf::Vector2f((tu + 1) * tileSize.x, tv * tileSize.y);
-                quad[2].texCoords = sf::Vector2f((tu + 1) * tileSize.x, (tv + 1) * tileSize.y);
-                quad[3].texCoords = sf::Vector2f(tu * tileSize.x , (tv + 1) * tileSize.y);
-            }
-
-        return true;
-    }
-
-private:
-
-    virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const
-    {
-
-        states.transform *= getTransform();
-        
-        states.texture = &m_tileset;
-
-        target.draw(m_vertices, states);
-    }
-
-    sf::VertexArray m_vertices;
-    sf::Texture m_tileset;
-};
-
+#include "tilemap.hpp"
 
 
 int main()
 {
     sf::Texture texture;
-    if (!texture.loadFromFile(resourcePath() + "meuf_face.png")) {
+    if (!texture.loadFromFile(/* resourcePath() + */ "Sprites/meuf_face.png")) {
         return EXIT_FAILURE;
     }
     sf::Sprite shape(texture);
@@ -97,7 +45,7 @@ int main()
 
     TileMap map;
     
-    if (!map.load("eau_trouble.png", sf::Vector2u(16, 16), level, 34, 11)) return -1;
+    if (!map.load("Sprites/eau_trouble.png", sf::Vector2u(16, 16), level, 34, 11)) return -1;
 
     const int level2[] =
     {
@@ -140,7 +88,7 @@ int main()
 
     TileMap bigmap;
     
-    if (!bigmap.load("herbe.png", sf::Vector2u(16, 16), level2, 34,34 )) return -1;
+    if (!bigmap.load("Sprites/herbe.png", sf::Vector2u(16, 16), level2, 34,34 )) return -1;
     
     while (window.isOpen())
     {
@@ -158,22 +106,22 @@ int main()
                 if (event.key.code == sf::Keyboard::Left) {
                     shape.move(-16, 0);
                     view.move(-16, 0);
-                    if (!texture.loadFromFile(resourcePath() + "meuf_gauche.png")) return EXIT_FAILURE;
+                    if (!texture.loadFromFile(/* resourcePath() +  */"Sprites/meuf_gauche.png")) return EXIT_FAILURE;
                 }
                 if (event.key.code == sf::Keyboard::Right) {
                     shape.move(16, 0);
                     view.move(16, 0);
-                    if (!texture.loadFromFile(resourcePath() + "meuf_droite.png")) return EXIT_FAILURE;
+                    if (!texture.loadFromFile(/* resourcePath() + */ "Sprites/meuf_droite.png")) return EXIT_FAILURE;
                 }
                 if (event.key.code == sf::Keyboard::Up) {
                     shape.move(0, -16);
                     view.move(0, -16);
-                    if (!texture.loadFromFile(resourcePath() + "meuf_dos.png")) return EXIT_FAILURE;
+                    if (!texture.loadFromFile(/* resourcePath() +  */"Sprites/meuf_dos.png")) return EXIT_FAILURE;
                 }
                 if (event.key.code == sf::Keyboard::Down) {
                     shape.move(0, 16);
                     view.move(0,16);
-                    if (!texture.loadFromFile(resourcePath() + "meuf_face.png")) return EXIT_FAILURE;
+                    if (!texture.loadFromFile(/* resourcePath() +  */"Sprites/meuf_face.png")) return EXIT_FAILURE;
                 }
             }
         }
