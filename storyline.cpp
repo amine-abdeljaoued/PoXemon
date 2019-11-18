ceedc369e72f863e5dd2eb6a99a28230b6ee63cf
julienluzzatto <45912458+julienluzzatto@users.noreply.github.com>
2019-11-18 14:37:04 +0100

    Storyline
    Storyline by Clement, Maxence, Julien

---
+++ storyline.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+/* Initial screen: beginning of the game; we start as in the real Pokemon game:
+
+Hi! Sorry to keep you waiting!
+Welcome to the world of [Pokemon]
+My name is [Didy]
+But everyone calls me [Pokemon Professor]"
+This is what we call [Pokemon]
+The world is widely inhabited by creatures called [Pokemon]
+We humas live alongside Pokemon, at times as friendly playmates, and at
+times as cooperative workmates,
+And sometimes, we band together and battle others like us.
+But despite our closeness, we don't know everything about [Pokemon].
+In fact, there are many, many secrets surrounding [Pokemon].
+To unravel [Pokemon] mysteries, I've been undertaking research.
+That's what I do. And you are?
+Are you a boy? Or are you a girl?
+*/
