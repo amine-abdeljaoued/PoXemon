e0c19b1c1707cc39b19a059a691b5149b12721fa
benjamin-montagnes <benji.montagnes@gmail.com>
2019-11-28 08:23:36 +0100

    update

---
+++ campusmap/map.cpp
@@ -0,0 +1,53 @@
+#include "map.hpp"
+#include "const.hpp"
+#include "trainer.hpp"
+#include <SFML/Audio.hpp>
+#include <SFML/Graphics.hpp>
+#include <unistd.h>
+#include <stdio.h>
+#include <iostream>
+#include <list>
+using namespace std;
+
+Map::Map(int i)
+{
+    if(i==1)
+    {
+        if (!background.load("Sprites/tileset1.png", sf::Vector2u(16, 16), level, 34, 34)){
+            cout << "Error loading the sprite";
+        }
+       
+        Npc* toto = new  Npc("Sprites/NPC2.png",70,50,0.5f,200,212);
+        npcs.push_back(toto) ;
+    }
+    
+}
+
+Map::~Map()
+{
+    for (list<Npc*>::iterator it = npcs.begin(); it != npcs.end(); it++)
+    {
+        delete *it;
+    }
+}
+
+void Map::draw(sf::RenderWindow &window, Trainer &trainer) const {
+    
+    sf::Vector2f pos =trainer.getPos();
+    window.draw(background);
+    for (auto const& np : npcs) 
+    {
+        sf::Vector2f pos2 = (*np).getPos(); //Here we study the respective positions of the character and the npcs 
+                                            //to check in which order we draw them
+        if(pos.y> pos2.y){
+            (*np).draw(window);
+            trainer.draw(window);
+        }
+        else{
+            trainer.draw(window);
+            (*np).draw(window);
+        }
+        
+    } 
+    
+}
---
+++ campusmap/npc.cpp
@@ -0,0 +1,28 @@
+#include "npc.hpp"
+#include <SFML/Audio.hpp>
+#include <SFML/Graphics.hpp>
+#include <unistd.h>
+#include <iostream>
+#include <stdio.h>
+#include <string>
+#include <deque>
+using namespace std;
+
+Npc::Npc(string pathName, int sheetRectX,int sheetRectY, float scale , int posX, int posY) {
+    if (!textureNpc.loadFromFile(/* resourcePath() + */ pathName)) {
+        cout << "Error loading sprite";
+    }
+    (spriteNpc).setTexture(textureNpc);
+    spriteNpc.setTextureRect(sf::IntRect(0,0,sheetRectX,sheetRectY));
+    spriteNpc.setScale(sf::Vector2f(scale, scale));
+    spriteNpc.setPosition(posX, posY);
+}
+
+void Npc::draw(sf::RenderWindow &window) const {
+    window.draw(spriteNpc);
+}
+
+sf::Vector2f Npc::getPos(){
+    sf::Vector2f position = (spriteNpc).getPosition();
+    return position;
+}
