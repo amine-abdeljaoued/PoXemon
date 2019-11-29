7a56d7ed747dfa58db66c934fa1e509b17765dae
jiahehu0312 <jiahezhu0312@gmail.com>
2019-11-29 09:59:11 +0100

    Opponent can move randomly
    Random function on the direction of the movement and the Opponent is always moving

--- Opponent.h
+++ Opponent.h
@@ -8,7 +8,9 @@ class Opponent: public Pokemon{
 protected:
     Pokemon player;
 public:
-    Opponent(float xstart, float ystart, float h, float v, float hp, std::string path, float pokemon_rarity):Pokemon(xstart, ystart, h, v, hp, path,pokemon_rarity){};
+    Opponent(Pokemon player_poke,float xstart, float ystart, float h, float v, float hp, std::string path, float pokemon_rarity):Pokemon(xstart, ystart, h, v, hp, path,pokemon_rarity){
+        player=player_poke;
+    };
     //void updateMovement(float& deltaTime);
     void update(float& deltaTime);
     void move(float& deltaTime);
