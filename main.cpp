f3ad83c7cba332673c6976da01c602e9d8fd012d
anyafries <anya.fries@gmail.com>
2019-12-05 05:39:39 -0500

    Adding states to main

--- main.cpp
+++ main.cpp
@@ -15,81 +15,98 @@
 float groundY = 300.0f; //Cannot go below this height
 float groundX = 1000.f;
 
-int main ()
-{
-    sf::RenderWindow window(sf::VideoMode(1400, 700), "My window");
-
-	// -----------------------------------------------------------------
-	// This gives us a background that is the same size as the window
-	sf::Texture BackgroundTexture;
-	sf::Sprite background;
+void initialise_background(sf::RenderWindow& window, std::string path, sf::Sprite& background, sf::Texture& texture){
+	// Function that sets up our background to be the same size as the window
+	// Need to initialise the sprite and texture of the background before though
 	sf::Vector2u TextureSize;  //Added to store texture size.
 	sf::Vector2u WindowSize;   //Added to store window size.
 
-	if(!BackgroundTexture.loadFromFile("Images/grassbg.png")){
-                std::cout<<"BG didn't load"<<std::endl;
-	}
+	if(!texture.loadFromFile(path)){std::cout<<"Background didn't load"<<std::endl;}
 	else
 	{
-		TextureSize = BackgroundTexture.getSize(); //Get size of texture.
-		WindowSize = window.getSize();             //Get size of window.
+		TextureSize = texture.getSize(); 	//Get size of texture.
+		WindowSize = window.getSize();      //Get size of window.
 
 		float ScaleX = (float) WindowSize.x / TextureSize.x;
 		float ScaleY = (float) WindowSize.y / TextureSize.y;     //Calculate scale.
 
-		background.setTexture(BackgroundTexture);
+		background.setTexture(texture);
 		background.setScale(ScaleX, ScaleY);      //Set scale.  
   	}
-	// -----------------------------------------------------------------
-	
+}
+
+// We declare a global variable for fights which shows the game state
+// 1 = start menu
+// 2 = normal fight mode
+// 3 = pokeball landed in centre -- to be implemented
+// 4 = caught the pokemon
+// 5 = we died -> choose another or quit
+// 6 = opponent dies
+int state = 2;
+
+
+int main ()
+{
 	Player eevee(10.f, groundY, 200.f, 500.f, 100, "Images/Pokemon_Images/vulari.png", 2, "Eevee");
 	Opponent opponent1(1000.f,groundY,200.f, 800.f, 100, "Images/Pokemon_Images/urach.png", 2, "Opponent");
 	eevee.set_enemy(&opponent1);
 	opponent1.set_enemy(&eevee);
+	
+    sf::RenderWindow window(sf::VideoMode(1400, 700), "My window");
 
-    float deltaTime = 0.0f;
+	if (state==1){
+		//...
+	}
+	
+	if (state == 2){
+		sf::Texture BackgroundTexture;
+		sf::Sprite background;
+		initialise_background(window, "Images/grassbg.png", background, BackgroundTexture);
 
-    sf::Clock clock;
-    sf::Clock clock_regenerate_bullets;
-    sf::Time elapsed;
-    
-    sf::Clock clock2;
-    sf::Time elapsed2;
-    
-    Backpack bag;
-	static int counter=0;
+		float deltaTime = 0.0f;
 
-	// run the program as long as the window is open
-	while (window.isOpen())
-	{
-		deltaTime = clock.restart().asSeconds();
-		elapsed = clock_regenerate_bullets.getElapsedTime();
-        elapsed2 = clock2.getElapsedTime();
-
-		sf::Event event;
-		while (window.pollEvent(event))
-		{
-			if (event.type == sf::Event::Closed)
-			{
-				std::cout << "Goodbye" << std::endl;
-				window.close();
-			}
-		}
-
-        // clear the window with black color - need to clear before drawing anything (overlap)
-        eevee.update(deltaTime, window, clock_regenerate_bullets, elapsed, groundY);
-	 	opponent1.update(deltaTime, window, clock_regenerate_bullets, elapsed, groundY);
-
-		bag.Pokeball_shoot(deltaTime, window, clock2, elapsed2);
-		window.clear(sf::Color::Blue);
-		window.draw(background);
-        bag.draw(window);
-        eevee.draw(window);
-		opponent1.draw(window);
-
-		//std::cout << counter++ << std::endl;
+		sf::Clock clock;
+		sf::Clock clock_regenerate_bullets;
+		sf::Time elapsed;
 		
-		window.display();
+		sf::Clock clock2;
+		sf::Time elapsed2;
+		
+		Backpack bag;
+		static int counter=0;
+
+		// run the program as long as the window is open
+		while (window.isOpen())
+		{
+			deltaTime = clock.restart().asSeconds();
+			elapsed = clock_regenerate_bullets.getElapsedTime();
+			elapsed2 = clock2.getElapsedTime();
+
+			sf::Event event;
+			while (window.pollEvent(event))
+			{
+				if (event.type == sf::Event::Closed)
+				{
+					std::cout << "Goodbye" << std::endl;
+					window.close();
+				}
+			}
+
+			// clear the window with black color - need to clear before drawing anything (overlap)
+			eevee.update(deltaTime, window, clock_regenerate_bullets, elapsed, groundY);
+			opponent1.update(deltaTime, window, clock_regenerate_bullets, elapsed, groundY);
+
+			bag.Pokeball_shoot(deltaTime, window, clock2, elapsed2);
+			window.clear(sf::Color::Blue);
+			window.draw(background);
+			bag.draw(window);
+			eevee.draw(window);
+			opponent1.draw(window);
+
+			//std::cout << counter++ << std::endl;
+			
+			window.display();
+		}
     }
     return 0;
 }
