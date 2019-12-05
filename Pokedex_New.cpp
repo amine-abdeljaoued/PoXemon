6bc60f78e52bc6e52af98d79a1c6b5dd60231377
amcalud <56112280+amcalud@users.noreply.github.com>
2019-12-05 22:51:18 +0100

    Fully functional pokedex
    Needs to be linked to the map

--- Pokedex_New.cpp
+++ Pokedex_New.cpp
@@ -8,37 +8,42 @@ void main(int argc, char** argv[]) {
 	sf::RenderWindow window(sf::VideoMode(880, 720), "Pokedex?");
 
 	sf::Texture PokedexTexture;
-	PokedexTexture.loadFromFile("test.png");
-
+	PokedexTexture.loadFromFile("test2.png");
 	sf::Sprite Pokedex(PokedexTexture);
 
-
-
 	Pokedex.setOrigin(0.f,0.f);
-
 
 	sf::Texture ArrowsTexture;
 	ArrowsTexture.loadFromFile("arrows.png");
 	sf::Sprite Arrows(ArrowsTexture);
 	Arrows.setOrigin(-700.f, -260.f);
 
-
 	sf::Texture element_air;
 	element_air.loadFromFile("element_air.png");
 	sf::Sprite Air(element_air);
-	Air.setOrigin(-730.f, -40.f);
-
+	
 	sf::Texture element_earth;
 	element_earth.loadFromFile("element_earth.png");
 	sf::Sprite Earth(element_earth);
-	Earth.setOrigin(-730.f, -560.f);
+	
+	sf::Texture element_fire;
+	element_fire.loadFromFile("element_fire.png");
+	sf::Sprite Fire(element_fire);
+
+	sf::Texture element_water;
+	element_water.loadFromFile("element_water.png");
+	sf::Sprite Water(element_water);
 
 
 	int element = 0;
 
 
 	sf::Vector2f increment(0.f, 240.f);
-
+	sf::Vector2f increment_big(0.f, 1440.f);
+	sf::Vector2f increment_medium(0.f, 480.f);
+	sf::Vector2f transition(880.f, 0.f);
+	sf::Vector2f big_transition(3520.f, 0.f);
+	sf::Vector2f temp(0.f, 0.f);
 
 
 	while (window.isOpen()) {
@@ -57,18 +62,42 @@ void main(int argc, char** argv[]) {
 						if (Pokedex.getPosition().y + increment.y < 1) {
 							Pokedex.setPosition(Pokedex.getPosition() + increment);
 						}
+						else {
+							if (element == 0 || element == 1) {Pokedex.setPosition(Pokedex.getPosition() - increment_big);}
+							else {Pokedex.setPosition(Pokedex.getPosition() - increment_medium);}
+						}
 					}
 					else if (360 <= position.y && position.y <= 440) { //down
 						Pokedex.setPosition(Pokedex.getPosition() - increment);
+						if (element == 0 || element == 1) {
+							if (Pokedex.getPosition().y - increment.y < -increment_big.y -240) {Pokedex.setPosition(Pokedex.getPosition() + increment_big + increment);}
+						}
+						else {
+							if (Pokedex.getPosition().y - increment.y < -increment_medium.y - 240) {Pokedex.setPosition(Pokedex.getPosition() + increment_medium + increment);}
+						}
 						
 					}
 
-					else if (40 <= position.y && position.y <= 150) {
-						std::cout << "air" << '\n';
+					else if (40 <= position.y && position.y <= 150) { //next element
+						element = element + 1;
+						temp.x = Pokedex.getPosition().x - transition.x;
+						Pokedex.setPosition(temp);
+						if (element == 4) {
+							element = 0;
+							temp.x = Pokedex.getPosition().x + big_transition.x;
+							Pokedex.setPosition(temp);
+						}
 					}
 
 					else if (560 <= position.y && position.y <= 690) {
-						std::cout << "earth" << '\n';
+						element = element - 1;
+						temp.x = Pokedex.getPosition().x + transition.x;
+						Pokedex.setPosition(temp);
+						if (element == -1) {
+							element = 3;
+							temp.x = Pokedex.getPosition().x - big_transition.x;
+							Pokedex.setPosition(temp);
+						}
 					}
 				}
 
@@ -88,98 +117,33 @@ void main(int argc, char** argv[]) {
 		window.draw(Pokedex); // Drawing our sprite.
 		window.draw(Arrows);
 
-		window.draw(Air);
-		window.draw(Earth);
+		if (element == 0) {
+			Fire.setOrigin(-730.f, -40.f);
+			Water.setOrigin(-730.f, -560.f);
+			window.draw(Fire);
+			window.draw(Water);
+		}
+		else if (element == 1) {
+			Air.setOrigin(-730.f, -40.f);
+			Earth.setOrigin(-730.f, -560.f);
+			window.draw(Air);
+			window.draw(Earth);
+		}
+		else if (element == 2) {
+			Water.setOrigin(-730.f, -40.f);
+			Fire.setOrigin(-730.f, -560.f);
+			window.draw(Water);
+			window.draw(Fire);
+		}
+		else {
+			Earth.setOrigin(-730.f, -40.f);
+			Air.setOrigin(-730.f, -560.f);
+			window.draw(Earth);
+			window.draw(Air);
+		}
+
 		window.display();
 
 		
 	}
 }
-
-/*
-		if (sf::Keyboard::isKeyPressed(sf::Keyboard::W)) //up
-		{
-			increment.y = 4;
-			if (mushroom.getPosition().y + increment.y > 0) {
-				increment.y = 0.f;
-			}
-			
-		}
-
-		else if (sf::Keyboard::isKeyPressed(sf::Keyboard::S)) //up
-		{
-			increment.y = -4;
-			
-		}
-*/
-
-/*
-			else if (event.type == sf::Event::MouseWheelMoved)
-			{
-				std::cout << event.mouseWheel.delta << '\n';
-				increment.y = event.mouseWheel.delta;
-				increment.y = increment.y / 50;
-			}
-			*/
-
-/*
-	float gridSizeF = 100.f;
-	unsigned gridSizeU = static_cast<unsigned>(gridSizeF);
-	float dt = 0.f;
-	sf::Clock dtClock;
-
-	sf::RenderWindow window(sf::VideoMode(1920, 1080), "SFML works!");
-
-
-	sf::View view;
-	view.setSize(1920.f, 1080.f);
-	view.setCenter(window.getSize().x / 2.f, window.getSize().y / 2.f);
-	float viewSpeed = 300.f;
-
-	sf::RectangleShape shape(sf::Vector2f(gridSizeF, gridSizeF));
-
-	while (window.isOpen())
-	{
-		dt = dtClock.restart().asSeconds();
-
-
-		sf::Event event;
-		while (window.pollEvent(event))
-		{
-			if (event.type == sf::Event::Closed)
-				window.close();
-		}
-
-		if (sf::Keyboard::isKeyPressed(sf::Keyboard::A))
-		{
-			view.move(viewSpeed*dt, 0.f);
-		}
-
-		else if (sf::Keyboard::isKeyPressed(sf::Keyboard::D))
-		{
-			view.move(-viewSpeed * dt, 0.f);
-		}
-
-		else if (sf::Keyboard::isKeyPressed(sf::Keyboard::W))
-		{
-			view.move(0.f, viewSpeed * dt);
-		}
-
-		else if (sf::Keyboard::isKeyPressed(sf::Keyboard::S))
-		{
-			view.move(0.f, -viewSpeed * dt);
-		}
-
-
-
-		window.clear();
-		window.setView(view);
-
-		window.draw(shape);
-
-		window.setView(window.getDefaultView());
-
-
-		window.display();
-	}
-	*/
