e71891ed9f103c9c372f7f5a0f60a48f2d9c533c
benjamin-montagnes <benji.montagnes@gmail.com>
2019-12-12 09:39:37 +0100

    linked the intro with the map game

--- campusmap/main.cpp
+++ campusmap/main.cpp
@@ -9,35 +9,51 @@
 #include "const.hpp"
 #include "npc.hpp"
 #include "map.hpp"
-
-
+#include "menu.hpp"
+#include "intro.hpp"
 
 int main()
 {
+    menu("main_menu.jpg");
+    intro("professor1.png",1,0);
+    intro("professor4.png",1,1);
+    intro("professor3.png",0,2);
+    intro("professor5.png",0,3);
+    intro("professor2.png",0,4);
+    intro("professor6.png",1,5);
+    intro("professor7.png",1,6);
+    intro("professor1.png",0,7);
+    intro("professor4.png",0,8);
+    intro("professor6.png",1,9);
+    intro("professor5.png",1,10);
+    intro("professor3.png",0,11);
+    intro("professor2.png",0,12);
+    choice("professor0.png",1,13);
+    if(choice("professor0.png",1,14)==1){
+        intro("professor7.png",0,15);
+    }
+    else{
+    intro("professor4.png",0,16);
+    intro("professor1.png",1,17);
+        int a = choose("professor0.png",1);
+        if(a==1){
+            intro("professor5.png",0,18);
+        }
+        else if(a==2){
+            intro("professor5.png",0,19);
+        }
+        else if(a==3){
+            intro("professor5.png",0,20);
+        }
+    }
+    
+    
     std::string map_name = "first";
-
-    sf::RenderWindow window(sf::VideoMode(544, 544), "Tilemap");
+    sf::RenderWindow window(sf::VideoMode().getDesktopMode(), "Tilemap");
     sf::View view(sf::Vector2f(272, 272), sf::Vector2f(544, 544));
     view.zoom(0.5f);
+    
     window.setPosition(sf::Vector2i(0, 0));
-
-   /*  sf::Font font;
-    if (!font.loadFromFile( *//* resourcePath() + */ /* "Sprites/sansation.ttf")) {
-        return EXIT_FAILURE;
-    } */
-    /* sf::Text text("Bonjour, je suis\nvotre premier PNJ", font, 24);
-    text.setFillColor(sf::Color::Black);
-    text.setPosition(100, 100);
-    text.setStyle(sf::Text::Bold);
-    sf::RectangleShape rectangle(sf::Vector2f(230, 60));
-    rectangle.setPosition(90, 100); */
-
-    /* sf::Music music;
-    if (!music.openFromFile(resourcePath() + "town_1.ogg")) {
-        return EXIT_FAILURE;
-    }
-    music.play(); */
-
 
     //Variables for the character and declaration of him/ we need to put it in constructor
     float playerMovementSpeed = 2;
@@ -48,7 +64,6 @@ int main()
     
     // const for the transition
     sf::Clock clock;
-    
     window.setFramerateLimit(30);
     
     Map map1(window, map_name);
@@ -57,20 +72,14 @@ int main()
     {
         sf::Event event;
         window.pollEvent(event);
+        
         if(event.type == sf::Event::Closed) window.close();
         
         window.clear(sf::Color(112,200,160));
-
         sf::Time elapsed1 = clock.getElapsedTime();
-        
-        
         map1.draw(window,view, Arthur, clock, event, map_name);
-        
         Arthur.displacement(event,view, case_num);
-        
         window.setView(view);
-     
-
         window.display();
     }
 
