effa73ee583cc9779e6e439abe39df8cf33785e1
benjamin-montagnes <benji.montagnes@gmail.com>
2019-12-12 10:26:31 +0100

    upload my work

--- campusmap/main.cpp
+++ campusmap/main.cpp
@@ -29,30 +29,30 @@ std::string toString(T arg){
 
 int main()
 {
-    menu("main_menu.jpg");
-    intro("professor1.png",1,0);
-    intro("professor4.png",1,1);
-    intro("professor3.png",0,2);
-    intro("professor5.png",0,3);
-    intro("professor2.png",0,4);
-    intro("professor6.png",1,5);
-    intro("professor7.png",1,6);
-    intro("professor1.png",0,7);
-    intro("professor4.png",0,8);
-    intro("professor6.png",1,9);
-    intro("professor5.png",1,10);
-    intro("professor3.png",0,11);
-    intro("professor2.png",0,12);
-    choice("professor0.png",1,13);
-    if(choice("professor0.png",1,14)==1) intro("professor7.png",0,15);
-    else{
-    intro("professor4.png",0,16);
-    intro("professor1.png",1,17);
-        int a = choose("professor0.png",1);
-        if(a==1) intro("professor5.png",0,18);
-        else if(a==2) intro("professor5.png",0,19);
-        else if(a==3) intro("professor5.png",0,20);
-        }
+//    menu("main_menu.jpg");
+//    intro("professor1.png",1,0);
+//    intro("professor4.png",1,1);
+//    intro("professor3.png",0,2);
+//    intro("professor5.png",0,3);
+//    intro("professor2.png",0,4);
+//    intro("professor6.png",1,5);
+//    intro("professor7.png",1,6);
+//    intro("professor1.png",0,7);
+//    intro("professor4.png",0,8);
+//    intro("professor6.png",1,9);
+//    intro("professor5.png",1,10);
+//    intro("professor3.png",0,11);
+//    intro("professor2.png",0,12);
+//    choice("professor0.png",1,13);
+//    if(choice("professor0.png",1,14)==1) intro("professor7.png",0,15);
+//    else{
+//    intro("professor4.png",0,16);
+//    intro("professor1.png",1,17);
+//        int a = choose("professor0.png",1);
+//        if(a==1) intro("professor5.png",0,18);
+//        else if(a==2) intro("professor5.png",0,19);
+//        else if(a==3) intro("professor5.png",0,20);
+//        }
 
     
     std::string map_name = "first";
