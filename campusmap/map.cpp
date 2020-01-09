52b0c6fafc3f9883a2fb588365f8ebb4bf3107f1
benjamin-montagnes <benji.montagnes@gmail.com>
2020-01-09 09:00:26 +0100

    updated maps

--- campusmap/map.cpp
+++ campusmap/map.cpp
@@ -891,6 +891,57 @@ void Map::illuShop(sf::RenderWindow &window){
     }
 }
 
+void Map::illuTunnelR(sf::RenderWindow &window){
+    if (map_name == "fourth"){
+        for(int i = 0; i < 5; i++){
+            sf::Sprite sprite;
+            (sprite).setTexture(texture_2);
+            (sprite).setTextureRect(sf::IntRect(409, 18 + 17 * i, 16, 16));
+            sprite.setPosition(528, 416 + 16 * i );
+            window.draw(sprite);
+            
+            sf::Sprite sprite2;
+            (sprite2).setTexture(texture_2);
+            (sprite2).setTextureRect(sf::IntRect(426, 18 + 17 * i, 16, 16));
+            sprite2.setPosition(544, 416 + 16 * i );
+            window.draw(sprite2);
+        }
+        
+        for(int k = 0; k<2; k++){
+            sf::Sprite sprite;
+            (sprite).setTexture(texture_2);
+            (sprite).setTextureRect(sf::IntRect(392, 35 + 17 * k, 16, 16));
+            sprite.setPosition(512, 432 + 16 * k );
+            window.draw(sprite);
+        }
+    }
+}
 
+
+void Map::illuTunnelL(sf::RenderWindow &window){
+    if (map_name == "home"){
+        for(int i = 0; i < 5; i++){
+            sf::Sprite sprite;
+            (sprite).setTexture(texture_2);
+            (sprite).setTextureRect(sf::IntRect(494, 18 + 17 * i, 16, 16));
+            sprite.setPosition(0, 160 + 16 * i );
+            window.draw(sprite);
+            
+            sf::Sprite sprite2;
+            (sprite2).setTexture(texture_2);
+            (sprite2).setTextureRect(sf::IntRect(426, 18 + 17 * i, 16, 16));
+            sprite2.setPosition(-16, 160 + 16 * i );
+            window.draw(sprite2);
+        }
+        
+        for(int k = 0; k<2; k++){
+            sf::Sprite sprite;
+            (sprite).setTexture(texture_2);
+            (sprite).setTextureRect(sf::IntRect(511, 35 + 17 * k, 16, 16));
+            sprite.setPosition(16, 176 + 16 * k );
+            window.draw(sprite);
+        }
+        
+    }
 }
 
