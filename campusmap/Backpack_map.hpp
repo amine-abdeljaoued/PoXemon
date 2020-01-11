ca1166568d974b1e2974c97177294cb84ef8a0f9
arthur-collette <arthur.collette@polytechnique.edu>
2020-01-11 19:33:01 +0100

    bag by clement <3

--- campusmap/Backpack_map.hpp
+++ campusmap/Backpack_map.hpp
@@ -22,7 +22,16 @@ public:
     void Moveup(sf::Event &event);
     void Movedown(sf::Event &event);
     
+    void Moveup_Pokemon(sf::Event &event);
+    void Movedown_Pokemon(sf::Event &event);
+    
+    void Moveleft(sf::Event &event);
+    void Moveright(sf::Event &event);
+    
+    //Information for the Fights
     std::map<std::string, int> inventory;
+    Backpack_Pokemon* backpack_pokemons[3];
+    
     
     sf::Text text;
     sf::Text n_item;
@@ -32,17 +41,42 @@ public:
     sf::Sprite rectangle;
     sf::Sprite money;
     
+    //Sprites for Pokemon Display
+      
+    //Slots
+    sf::Sprite empty_slot;
+    sf::Sprite not_selected_slot;
+    sf::Sprite selected_slot;
+      
+    //Pokeballs
+    sf::Sprite poke_close;
+    sf::Sprite poke_open;
+      
+    //Pokemon Names
+    sf::Text poke_name;
+      
+    //Pokemon Levels
+    sf::Text level;
+      
+    //Type
+    sf::Sprite earth;
+    sf::Sprite water;
+    sf::Sprite air;
+    sf::Sprite fire;
+    
     int num_item();
     int getPresseditem();
     int Selecteditem;
+    int Selecteditem_pokemon;
     int number; //contains the actual number of items
     int wallet;
-    
-    Backpack_Pokemon* backpack_pokemons[3];
+    int actual_column;
+    float health_poke;
     
 private:
     void draw_background(sf::RenderWindow &window, sf::View &view, sf::Event &event, Trainer &trainer);
     void draw_menu(sf::RenderWindow &window, sf::View &view, sf::Event &event, Trainer &trainer);
+    void draw_pokemon(sf::RenderWindow &window, sf::View &view, sf::Event &event, Trainer &trainer);
     
     int capacity;
     
@@ -51,6 +85,8 @@ private:
     sf::Texture texture_backpack;
     sf::Texture rectangle_texture;
     sf::Texture money_texture;
+    sf::Texture slots;
+    sf::Texture types;
     sf::Font font;
     sf::Font font_menu;
     sf::Text menu[7];
@@ -62,6 +98,8 @@ private:
     float width_i;
     bool arrow;
     bool release;
+    bool release_left;
+    bool release_right;
     
     sf::FloatRect getViewBounds(const sf::View &view);
 };
