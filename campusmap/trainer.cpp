b114afcbb4d07728273c90043af3cd9bfe735841
Benjamin Montagnes <49634784+benjamin-montagnes@users.noreply.github.com>
2020-01-12 16:44:18 +0100

    trainer update

--- campusmap/trainer.cpp
+++ campusmap/trainer.cpp
@@ -10,8 +10,7 @@
 #include <random>
 using namespace std;
 
-Trainer::Trainer( float Speed, int sheetRect, int sizeAnim)
-{
+Trainer::Trainer( float Speed, int sheetRect, int sizeAnim) {
     
     if (!texturePlayer.loadFromFile(/* resourcePath() + */ "Sprites/trainer.png")) {
         cout << "Error loading sprite";
@@ -33,7 +32,7 @@ Trainer::Trainer( float Speed, int sheetRect, int sizeAnim)
 //    fight_mode = 'w';
 }
 
-void Trainer::draw(sf::RenderWindow &window, sf::Event &event, sf::View &view){
+void Trainer::draw(sf::RenderWindow &window, sf::Event &event, sf::View &view) {
     window.draw(spritePlayer);
     displacement(event,view);
     fishing(window, view); //To put in map after condition of state fishing
@@ -41,21 +40,150 @@ void Trainer::draw(sf::RenderWindow &window, sf::Event &event, sf::View &view){
     rock(window, view);
 }
 
-void Trainer::setSpeed(sf::Event &event){
-    if ((event.type == sf::Event::KeyPressed)&&((event.key.code == sf::Keyboard::LShift)||(event.key.code == sf::Keyboard::RShift)))
-    {
+void Trainer::setSpeed(sf::Event &event) {
+    if ((event.type == sf::Event::KeyPressed)&&((event.key.code == sf::Keyboard::LShift)||(event.key.code == sf::Keyboard::RShift))) {
         playerMovementSpeed = 4;
         speedSprite = 340;
     }
-    if ((event.type == sf::Event::KeyReleased)&&((event.key.code == sf::Keyboard::LShift)||(event.key.code == sf::Keyboard::RShift)))
-    {
+    if ((event.type == sf::Event::KeyReleased)&&((event.key.code == sf::Keyboard::LShift)||(event.key.code == sf::Keyboard::RShift))) {
         playerMovementSpeed = 2;
         speedSprite = 0;
     }
 }
-sf::Vector2f Trainer::getPos(){
+sf::Vector2f Trainer::getPos() {
     sf::Vector2f position = (spritePlayer).getPosition();
     return position;
+}
+
+void Trainer::displacement(sf::Event &event, sf::View &view) {
+    sf::Vector2f position = (spritePlayer).getPosition();
+    int x = position.x + 16;
+    int y = position.y + 16;
+    setSpeed(event); 
+    if (state == "Walking") {
+        if(intern_clock.getElapsedTime().asMilliseconds() >= (float) 2000/30){
+            if (facingDirection == "Down") {
+                if (y >= b + 16) {
+                    state = "Stop";
+                    (spritePlayer).setTextureRect(sf::IntRect(2,2,sheetRect,sheetRect));
+                    counterWalk = 1;
+                }
+                else{
+                    (spritePlayer).move(0, playerMovementSpeed);
+                    (spritePlayer).setTextureRect(sf::IntRect(2 + counterWalk * (sheetRect+2)+speedSprite,2,sheetRect,sheetRect));
+                    view.move(0,playerMovementSpeed);
+                        
+                    counterWalk++;
+                    if(counterWalk==sizeAnim){
+                        counterWalk = 0;
+                    }
+                }
+            }
+            if (facingDirection == "Up") {
+                if ( y <= b - 16){
+                    state = "Stop";
+                    counterWalk = 1;
+                    (spritePlayer).setTextureRect(sf::IntRect(2+speedSprite,2 + (sheetRect+2),sheetRect,sheetRect));
+                }
+                else{
+                    (spritePlayer).move(0, -playerMovementSpeed);
+                    (spritePlayer).setTextureRect(sf::IntRect(2 + counterWalk * (2 + sheetRect)+speedSprite,2 + (sheetRect+2),sheetRect,sheetRect));
+                    view.move(0, -playerMovementSpeed);
+                         
+                    counterWalk++;
+                    if(counterWalk == sizeAnim){
+                        counterWalk = 0;
+                     }
+                }
+            }
+            if (facingDirection == "Right") {
+                if (x >= a + 16){// -10 because of initial position
+                        state = "Stop";
+                        counterWalk = 1;
+                    (spritePlayer).setTextureRect(sf::IntRect(2+speedSprite,2+ 3*(sheetRect+2),sheetRect,sheetRect));
+                }
+                else{
+                    (spritePlayer).move(playerMovementSpeed, 0);
+                    (spritePlayer).setTextureRect(sf::IntRect(2+counterWalk * (sheetRect+2)+speedSprite,2+ 3*(sheetRect+2),sheetRect,sheetRect));
+                    view.move(playerMovementSpeed, 0);
+                         
+                    counterWalk++;
+                    if(counterWalk == sizeAnim){
+                        counterWalk = 0;
+                     }
+                }
+            }
+            if (facingDirection == "Left") {
+                if (x <= a - 16 ){ // because of initial position
+                    state = "Stop";
+                    counterWalk = 1;
+                    (spritePlayer).setTextureRect(sf::IntRect(2+speedSprite,2+2*(sheetRect+2),sheetRect,sheetRect));
+                }
+                else{
+                    (spritePlayer).move(-playerMovementSpeed, 0);
+                    (spritePlayer).setTextureRect(sf::IntRect(2+counterWalk * (sheetRect+2)+speedSprite,2+2*(sheetRect+2),sheetRect,sheetRect));
+                    view.move(-playerMovementSpeed, 0);
+                    
+                    counterWalk++;
+                    if(counterWalk == sizeAnim){
+                        counterWalk = 0;
+                    }
+                }
+            }
+                intern_clock.restart();
+        }
+        else{
+            if (facingDirection == "Down") {
+                if (y >= b + 16) {
+                    state = "Stop";
+                    (spritePlayer).setTextureRect(sf::IntRect(2+speedSprite,2,sheetRect,sheetRect));
+                    counterWalk = 1;
+                }
+                else{
+                    (spritePlayer).move(0, playerMovementSpeed);
+                    (spritePlayer).setTextureRect(sf::IntRect(2 + counterWalk * (sheetRect+2)+speedSprite,2,sheetRect,sheetRect));
+                    view.move(0,playerMovementSpeed);
+                }
+            }
+            if (facingDirection == "Up") {
+                if ( y <= b - 16){
+                    state = "Stop";
+                    counterWalk = 1;
+                    (spritePlayer).setTextureRect(sf::IntRect(2+speedSprite,2+sheetRect+2,sheetRect,sheetRect));
+                }
+                else{
+                    (spritePlayer).move(0, -playerMovementSpeed);
+                    (spritePlayer).setTextureRect(sf::IntRect(2+counterWalk * (sheetRect+2)+speedSprite,2+sheetRect+2,sheetRect,sheetRect));
+                    view.move(0, -playerMovementSpeed);
+                }
+            }
+            if (facingDirection == "Right") {
+                if (x >= a + 16){// -10 because of initial position
+                        state = "Stop";
+                        counterWalk = 1;
+                    (spritePlayer).setTextureRect(sf::IntRect(2+speedSprite,2+3*(sheetRect+2),sheetRect,sheetRect));
+                }
+                else{
+                    (spritePlayer).move(playerMovementSpeed, 0);
+                    (spritePlayer).setTextureRect(sf::IntRect(2+counterWalk * (sheetRect+2)+speedSprite,2+3*(sheetRect+2),sheetRect,sheetRect));
+                    view.move(playerMovementSpeed, 0);
+                }
+            }
+
+            if (facingDirection == "Left") {
+                if (x <= a - 16 ){ // because of initial position
+                    state = "Stop";
+                    counterWalk = 1;
+                    (spritePlayer).setTextureRect(sf::IntRect(2+speedSprite,2+2*(sheetRect+2),sheetRect,sheetRect));
+                }
+                else{
+                    (spritePlayer).move(-playerMovementSpeed, 0);
+                    (spritePlayer).setTextureRect(sf::IntRect(2+counterWalk * (sheetRect+2)+speedSprite,2+2*(sheetRect+2),sheetRect,sheetRect));
+                    view.move(-playerMovementSpeed, 0);
+                }
+            }
+        }
+    }
 }
 
 sf::FloatRect Trainer::getViewBounds(const sf::View &view)
