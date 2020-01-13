4de206553b85043b514f643063cf78bc8d9513e0
clementsterlin <56109587+clementsterlin@users.noreply.github.com>
2020-01-13 13:58:38 +0100

    Update center.cpp

--- campusmap/center.cpp
+++ campusmap/center.cpp
@@ -3,20 +3,11 @@
 Center::Center(){
     Selecteditem = 0;
     arrow = true;
-    action_list = {"Heal", "Switch Pokemon", "Pokemon Box"};
-    pokemon_bag = {"jistolwer","auron","husabus"};
+    action_list = {"Heal", "Pokemon Box"};
     
     if (!font.loadFromFile(/* resourcePath() + */ "Fonts/sansation.ttf")) {
         std::cout<<"Problem"<<std::endl;
     }
-    
-    counter = 0;
-    interface = {action_list,pokemon_bag};
-        
-    //switch
-    sw = false;
-    swi1 = -1;
-    swi2 = -1;
 }
 
 void Center::draw(sf::RenderWindow &window, sf::View &view, sf::Event &event, Trainer &trainer, BackpackMap &backpack){
@@ -26,25 +17,17 @@ void Center::draw(sf::RenderWindow &window, sf::View &view, sf::Event &event, Tr
         
         sf::FloatRect viewBounds = getViewBounds(view);
         
-        sf::Text text[interface[counter].size()];
-        for (int i=0; i < interface[counter].size(); i++){
+        sf::Text text[2];
+        for (int i=0; i < 2; i++){
             text[i].setCharacterSize(10);
             text[i].setFillColor(sf::Color::Black);
             text[i].setFont(font);
             text[i].setPosition(4+ viewBounds.width/2 + viewBounds.left, viewBounds.top + 10*i + 4);
-            text[i].setString(interface[counter][i]);
+            text[i].setString(action_list[i]);
         }
         
         text[Selecteditem].setFillColor(sf::Color::Red);
         
-        if (sw == true){
-            if (swi1>=0){
-                text[swi1].setFillColor(sf::Color::Red);
-            }
-            if (swi2>=0){
-                text[swi2].setFillColor(sf::Color::Red);
-            }
-        }
         sf::RectangleShape rect(sf::Vector2f(viewBounds.width/2, viewBounds.height/3));
         rect.setFillColor(sf::Color::White);
         rect.setOutlineThickness(2);
@@ -52,12 +35,10 @@ void Center::draw(sf::RenderWindow &window, sf::View &view, sf::Event &event, Tr
         rect.setPosition(sf::Vector2f(viewBounds.left + viewBounds.width/2 -2, viewBounds.top + 2));
         window.draw(rect);
         
-        for(int i=0; i < interface[counter].size(); i++){
+        for(int i=0; i < 2; i++){
             window.draw(text[i]);
         }
         heal_pokemons(backpack, event, trainer);
-        switchMenu(window, backpack, event, view, trainer);
-        switchPokemon(window, backpack, event, view, trainer);
     }
 }
 
@@ -87,7 +68,7 @@ void Center::Moveup(sf::Event &event) {
 
 void Center::Movedown(sf::Event &event) {
     if (event.type == sf::Event::KeyPressed&&event.key.code == sf::Keyboard::Down&&arrow == true){
-        if (Selecteditem + 1 <= interface[counter].size() - 1) {
+        if (Selecteditem + 1 <= 1) {
             Selecteditem++;
         }
         arrow = false;
@@ -99,44 +80,12 @@ void Center::Movedown(sf::Event &event) {
 
     
 void Center::heal_pokemons(BackpackMap &backpack, sf::Event &event, Trainer &trainer) {
-    if (event.type == sf::Event::KeyPressed&&event.key.code == sf::Keyboard::X&&Selecteditem == 0&&counter == 0){
+    if (event.type == sf::Event::KeyPressed&&event.key.code == sf::Keyboard::X&&Selecteditem == 0){
         for (unsigned int i = 0; i < 3; i++) {
             if(backpack.backpack_pokemons[i]){
                 backpack.backpack_pokemons[i]->health = 100;
             }
         }
         trainer.state = "Stop";
-        counter = 0;
-        swi1 = -1;
-        swi2 = -1;
-        sw = false;
-    }
-}
-
-void Center::switchMenu(sf::RenderWindow &window, BackpackMap &backpack, sf::Event &event, sf::View &view, Trainer &trainer){
-    if (event.type == sf::Event::KeyPressed&&event.key.code == sf::Keyboard::X&&Selecteditem == 1&&counter == 0){
-        counter++;
-        Selecteditem = 0;
-    }
-}
-
-void Center::switchPokemon(sf::RenderWindow &window, BackpackMap &backpack, sf::Event &event, sf::View &view, Trainer &trainer){
-    if (counter == 1){
-        if (event.type == sf::Event::KeyPressed&&event.key.code == sf::Keyboard::X&&sw==false){
-            sw = true;
-            swi1 = Selecteditem;
-        }
-        if (event.type == sf::Event::KeyPressed&&event.key.code == sf::Keyboard::X&&sw==true&&Selecteditem!=swi1){
-            swi2 = Selecteditem;
-            
-            string temp = interface[counter][swi1];
-            interface[counter][swi1] = interface[counter][swi2];
-            interface[counter][swi2] = temp;
-            
-            
-            sw = false;
-            swi1 = -1;
-            swi2 = -1;
-        }
     }
 }
